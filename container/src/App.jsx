431b7e4d69df4edb8f504bb94af978aed20c91d8
Angel <jose.prieto@levva.io>
2023-09-10 09:13:18 -0300

    init repo

---
+++ container/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Button from "remoteHeader/Button"
+
+function App() {
+
+  return (
+    <>
+      <div>
+       Container
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button />
+    </>
+  )
+}
+
+export default App
---
+++ remote-header/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+export default defineConfig({
+  plugins: [react(), federation({
+    name: "remote-header",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./Button": "./src/components/Button/Button",
+    },
+    shared: ["react", "react-dom"],
+  }),],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  }
+  
+})
